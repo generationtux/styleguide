c117e49de368320996da9f56f8129cce1290e9d0
jarstingstall <jrarst01@gmail.com>
2017-05-09 16:02:53 -0400

    update HeroCarousel to HeroCarouselSlide

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,7 @@
 import OneUp from './content_blocks/OneUp.jsx'
 import TwoUpImgLeft from './content_blocks/TwoUpImgLeft.jsx'
 import TwoUpImgRight from './content_blocks/TwoUpImgRight.jsx'
-import HeroCarousel from './heros/HeroCarousel.jsx'
+import HeroCarouselSlide from './heros/HeroCarouselSlide.jsx'
 import HeroNoImg from './heros/HeroNoImg.jsx'
 import HeroTwoUpImgLeft from './heros/HeroTwoUpImgLeft.jsx'
 
@@ -9,7 +9,7 @@ export default {
   OneUp,
   TwoUpImgLeft,
   TwoUpImgRight,
-  HeroCarousel,
+  HeroCarouselSlide,
   HeroNoImg,
   HeroTwoUpImgLeft,
 }
